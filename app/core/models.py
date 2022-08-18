d91f9aa8a716df0e38c200dac251bf26129cf9e4
Patrick Louis <patricklouisw@gmail.com>
2022-08-17 21:16:28 -0400

    Creating Model User and db migration

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,36 @@
-from django.db import models # noqa
+"""
+Database Models.
 
-# Create your models here.
+AbstractBaseUser: contains functionality for auth system
+PermissionMixin: contains functionality for permissions & fields
+
+"""
+
+from unicodedata import name
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+
+class UserManager(BaseUserManager):
+    """Manager for users"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save, and return a new user"""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        pass
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
